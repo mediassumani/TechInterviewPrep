60ca52af3cb2e6d6538a399e00a864c41c18e9f2
Medi Assumani <mediassumani49@gmail.com>
2018-05-05 19:02:43 -0400

    adding exponent function

---
+++ PythonChallenges/integers_come_in_all_size.py
@@ -0,0 +1,31 @@
+
+# Challenge Name : Integers Come In All Sizes
+# Programmer : Medi Assumani
+# Task : Read four numbers, a,b,c,d, and print the result of a^b + c^d
+
+def exponent(base,power):
+    count = 1
+    result = base
+    if power == 0:
+        return 1
+    elif power == count:
+        return base
+    else:
+        while count < power:
+            result *= base
+            count++
+
+
+
+
+
+
+
+
+def compute(one,two,three,four):
+
+
+num_one = raw_input("Enter the first number : ")
+num_two = raw_input("Enter the second number : ")
+num_three = raw_input("Enter the third number : ")
+num_four = raw_input("Enter the fourth number : ")
