cbed0a113f353117451de3210db205a51d585b29
Medi Assumani <mediassumani49@gmail.com>
2018-10-29 08:00:43 -0700

    Create if_else.py

---
+++ PythonChallenges/if_else.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+N = int(input())
+if N % 2 != 0:
+    print("Weird")
+elif N % 2 == 0 and N >= 2 and N <= 5:
+    print("Not Weird")
+elif N % 2 == 0 and N >= 6 and N <= 20:
+    print("Weird")
+elif N % 2 == 0 and N > 20:
+    print("Not Weird")
