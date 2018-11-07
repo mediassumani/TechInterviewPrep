b90b537b7d9142a87ea1821695493356d29e7894
Medi Assumani <mediassumani49@gmail.com>
2018-11-07 07:03:45 -0800

    Update nested_lists.py

--- PythonChallenges/nested_lists.py
+++ PythonChallenges/nested_lists.py
@@ -1,1 +1,31 @@
 
+"""
+there are  5 students in this class whose names and grades are assembled to build the following list:
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+"""
+
+def find_second_lowest_score(arr):
+    scores = []
+    sorted_list = sorted(arr)
+    for student in arr:
+        scores.append(student[1])
+
+    sorted_scores = sorted(scores)
+    second_lowest = sorted_scores[1]
+
+    for student in sorted_list:
+        if student[1] is second_lowest:
+            print(student[0])
+
+
+
+def main():
+    students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41.0], ['Harsh', 39.0]]
+    find_second_lowest_score(students)
+
+
+
+if __name__ == "__main__":
+    main()
