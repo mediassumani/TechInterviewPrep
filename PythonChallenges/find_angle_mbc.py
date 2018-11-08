89bcd5a57f119bf582fdff760b523f34cd12025c
Medi Assumani <mediassumani49@gmail.com>
2018-11-08 08:04:48 -0800

    solved find_angle_mbc.py

--- PythonChallenges/find_angle_mbc.py
+++ PythonChallenges/find_angle_mbc.py
@@ -1,1 +1,15 @@
+import math
 
+def find_angle(lenght_ab, length_bc):
+    """ Finds the unknown angle in degree """
+    angle_unknown = math.atan2(lenght_ab, length_bc)
+    return int(round(math.degrees(angle_unknown)))
+
+def main():
+   ab = int(input())
+   bc = int(input())
+   print("{}°".format(find_angle(ab,bc)))
+
+
+if __name__ == '__main__':
+    main()
