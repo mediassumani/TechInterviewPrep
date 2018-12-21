fb1844fbaeab1cf9a3da94245c3dd7baf9104c62
Medi Assumani <mediassumani49@gmail.com>
2018-12-20 21:55:02 -0500

    solved long substring problem

---
+++ ProblemSolving/longest_substring.py
@@ -0,0 +1,41 @@
+"""
+PROBLEM:
+    - Given a string, find the length of the longest substring without repeating characters.
+    Time allocated : 20 minutes
+    Time used :
+"""
+
+
+def lengthOfLongestSubstring(s):
+
+    list_of_substrings = []
+    string = ""
+
+    if len(s) == 0:
+        return 0
+
+    for index in range(len(s)):
+
+        string += s[index]
+        next_character = s[index+1:index+2]
+        if next_character in string:
+            list_of_substrings.append(len(string))
+            string = ""
+
+    if len(list_of_substrings) != 0:
+        return max(list_of_substrings)
+
+    else:
+        return 1
+
+
+def main():
+
+    # print(lengthOfLongestSubstring("au"))
+    # print(lengthOfLongestSubstring("abcabcbb"))
+    # print(lengthOfLongestSubstring("bbbbb"))
+    # print(lengthOfLongestSubstring("pwwkew"))
+    print(lengthOfLongestSubstring(""))
+
+if __name__ == "__main__":
+    main()
