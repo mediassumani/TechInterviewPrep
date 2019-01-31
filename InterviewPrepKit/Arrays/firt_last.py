c6d56228e783cbb94d3b6501215a0bb6a60cfbe5
Medi Assumani <mediassumani49@gmail.com>
2019-01-31 07:25:45 -0800

    [Add] time and space complexity

--- InterviewPrepKit/Arrays/firt_last.py
+++ InterviewPrepKit/Arrays/firt_last.py
@@ -1,3 +1,8 @@
+'''Given an array of integers nums sorted in ascending order,
+ find the starting and ending position of a given target value.'''
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
 def find_first_last(arr, target):
 
     target_indexes = []
@@ -8,16 +13,6 @@ def find_first_last(arr, target):
     if len(target_indexes) == 0:
         return [-1, -1]
 
-
     first = target_indexes[0]
     last = target_indexes[len(target_indexes)-1]
     return [first, last]
-
-def main():
-    nums = [5,7,7,8,8,10]
-    target = 8
-
-    print(find_first_last(nums, target))
-
-if __name__ == "__main__":
-    main()
