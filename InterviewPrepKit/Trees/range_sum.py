80ce0da9de5af81d44bf7ca6f23365d70643c801
Medi Assumani <mediassumani49@gmail.com>
2019-02-02 09:44:32 -0800

    [Add] Time and space complexity

--- InterviewPrepKit/Trees/range_sum.py
+++ InterviewPrepKit/Trees/range_sum.py
@@ -3,25 +3,27 @@ Given the root node of a binary search tree, return the sum of values
 of all nodes with value between L and R (inclusive).
 The binary search tree is guaranteed to have unique values.
 
-Level  Medium
+Level  : Medium
 Time Allocated : 20 minutes
 Time Used : 32 minutes
+Time Complexity : O(n)
+Space Complexity : O(n)
 '''
 
 def rangeSumBST(self, root, L, R):
     '''Using DFS to find all values in range'''
 
-    list_of_results = []
+    sum = 0
     list_of_queues = deque([root])
 
     while len(list_of_queues) > 0:
         curr_node = list_of_queues.popleft()
         if curr_node.val >= L and curr_node.val <= R:
-            list_of_results.append(curr_node.val)
+            sum += curr_node.val
         if curr_node.left:
             list_of_queues.append(curr_node.left)
         if curr_node.right:
             list_of_queues.append(curr_node.right)
 
 
-    return(sum(list_of_results))
+    return(sum)
