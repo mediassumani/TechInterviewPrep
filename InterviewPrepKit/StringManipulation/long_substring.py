48010858137f2e08d2e5db6f7119adb7a99d3a8d
Medi Assumani <mediassumani49@gmail.com>
2019-06-20 11:24:30 -0700

    [Add] working solution

--- InterviewPrepKit/StringManipulation/long_substring.py
+++ InterviewPrepKit/StringManipulation/long_substring.py
@@ -1,17 +1,34 @@
 def is_unique(string):
+    
+    histogram = {}
+
+    for char in string:
+        if char not in histogram:
+            histogram[char] = 1
+        else:
+            return False
+
     return True
 
 def get_long_sbtrs(string):
+    ''' Finds the longest unique substring in a given string'''
 
     sbtrs = [] # hold all the substrings
 
     for i, char in enumerate(string):
-        outter_char = char
-        for index in range(index+1, len(string)):
-            curr_sbtr = string[i: index]
+        for index in range(i+1, len(string)):
+            curr_sbtr = string[i: index+1]
             if is_unique(curr_sbtr):
                 sbtrs.append(curr_sbtr)
 
-    return max(sbtrs)
+    return max(sbtrs, key=len)
 
+
+def main():
+    
+    word = "abccdef"
+    print(get_long_sbtrs(word))
+
+if __name__ == "__main__":
+    main()
 
