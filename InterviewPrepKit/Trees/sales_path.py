5f27fb05747217e3baf798253b4f67b1d2ab85fd
Medi Assumani <mediassumani49@gmail.com>
2019-07-12 11:39:03 -0700

    attempt to solve cheapest path problem

---
+++ InterviewPrepKit/Trees/sales_path.py
@@ -0,0 +1,41 @@
+'''
+Given a node rootNode, write a function getCheapestCost that calculates the minimal Sales Path cost in the tree.
+
+'''
+from collections import deque
+from binarytree import BinarySearchTree
+
+class Node(object):
+
+    def __init__(self, cost):
+        """Initialize this binary tree node with the given data."""
+        self.data = cost
+        self.children = []
+        self.parent = None
+
+
+
+def get_cheapest_cost(root):
+    
+    queue = deque([root])
+    
+    while len(queue) > 0:
+        
+        curr_node = queue.popleft()
+        if curr_node.children:
+
+            for child in curr_node.children:
+                queue.append(child)
+    
+def main():
+
+    root = Node(0)
+    get_cheapest_cost(root)
+
+if __name__ == "__main__":
+    main()
+
+    
+
+    
+    
