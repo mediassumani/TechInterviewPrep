e0f11c1235a4caadb7f0c55105b28002243b90c7
Medi Assumani <mediassumani49@gmail.com>
2019-07-19 19:15:02 -0700

    [Update] Djistrack algorithm... spudocode

--- InterviewPrepKit/Graphs/dijkstra_algorithm.py
+++ InterviewPrepKit/Graphs/dijkstra_algorithm.py
@@ -1,6 +1,22 @@
 
-def find_shortest_path():
+def find_shortest_path(graph, source):
     
-    visited = set()
-    unvisited = set()
-    shortest_path = 0
+    distance = {}
+    distance[source] = 0 # Set the initial distance to the source to 0
+
+    
+
+def main():
+    
+    graph = {}
+    graph["A"] = [("B", 1), ("D", 3), ("C", 4)]
+    graph["B"] = [("A", 1), ("C", 7)]
+    graph["C"] = [("B", 7), ("D", 6), ("A", 4)]
+    graph["D"] = [("A", 3), ("C", 6)]
+
+if __name__ == "__main__":
+    main()
+
+
+
+
