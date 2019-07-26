c3dcf9741c314f187f086d968e9297a8b3b52a97
Medi Assumani <mediassumani49@gmail.com>
2019-07-26 15:25:18 -0700

    [Add] solution to TempCheker problem from interview cake

--- InterviewPrepKit/temperature_cheker.py
+++ InterviewPrepKit/temperature_cheker.py
@@ -11,7 +11,7 @@ class TempTracker(object):
         self.low = None
         self.size = 0
 
-    def insert(self, degree):
+    def insert(self, degree): # O(1)
         ''' Records a new temperature'''
         
         if self.size == 0:
@@ -22,17 +22,36 @@ class TempTracker(object):
 
         elif degree < self.low:
             self.low = degree
+
+        if degree not in self.temperatures:
+            self.temperatures[degree] = 1
+        else:
+            self.temperatures[degree] += 1
+
+        self.size += 1
         
 
-    def get_max(self):
+    def get_max(self): # O(1)
+        ''' Get the highest temp ever recorded'''
         return self.high
     
-    def get_min(self):
+    def get_min(self): # O(1)
+        ''' Get the lowest temp ever recorded''' 
         return self.low
 
-
-    def get_mean(self):
-        pass
+    def get_mean(self): #(n)
+        ''' Get the mean of all the temperatures ever recorded'''
+        return sum(self.temperatures.keys()) / self.size/2
     
-    def get_mode(self):
-        pass
+    def get_mode(self): #(n)
+        ''' Get the most frequent temperature recorded '''
+
+        cur_val = 0
+        mode = None
+
+        for key,value in self.temperatures.items():
+            if value > cur_val:
+                cur_val = value
+                mode = key
+
+        return mode
